31f65278db09a33da1dcc76f586145ba34ba2286
Guilherme Piccioni <guilhermepiccioni@yahoo.com>
2022-03-09 09:18:16 -0300

    add users app

--- Makefile
+++ Makefile
@@ -2,4 +2,28 @@ DOCKER_COMPOSE := docker-compose
 
 .PHONY: build
 build:
-	$(DOCKER_COMPOSE) up -d --build
+	$(DOCKER_COMPOSE) up --build
+
+.PHONY: run
+run:
+	$(DOCKER_COMPOSE) up
+
+.PHONY: create_pytest_cov
+create_pytest_cov:
+	$(DOCKER_COMPOSE) exec web pytest --cov --cov-report=html
+
+.PHONY: pytest
+pytest:
+	$(DOCKER_COMPOSE) exec web pytest -x -v
+
+.PHONY: makemigrations
+makemigrations:
+	$(DOCKER_COMPOSE) exec web python manage.py makemigrations
+
+.PHONY: migrate
+migrate:
+	$(DOCKER_COMPOSE) exec web python manage.py migrate
+
+.PHONY: superuser
+superuser:
+	$(DOCKER_COMPOSE) exec web python manage.py createsuperuser
--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,13 +30,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Aps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ecommerce',
+    # My Applications
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User Model
+
+AUTH_USER_MODEL = "users.User"
